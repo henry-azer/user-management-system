ef6c5d2741c42d48eab9aba55429f633b4c94bd8
Henry-Azer <henryazer@outlook.com>
2022-04-20 00:49:28 +0200

    add backend spring app run

---
+++ user-management-system-backend/src/main/java/org/henry/UserManagementSystemBackendApplication.java
@@ -0,0 +1,13 @@
+package org.henry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserManagementSystemBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserManagementSystemBackendApplication.class);
+    }
+
+}
