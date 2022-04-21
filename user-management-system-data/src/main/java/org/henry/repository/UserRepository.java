be5372e7721f904e2a5bbb4537f1ca79a1115c6a
Henry-Azer <henryazer@outlook.com>
2022-04-21 05:48:26 +0200

    create user repository

--- user-management-system-data/src/main/java/org/henry/repository/UserRepository.java
+++ user-management-system-data/src/main/java/org/henry/repository/UserRepository.java
@@ -1,11 +1,9 @@
 package org.henry.repository;
 
-import org.henry.dao.UserDAO;
+import org.henry.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface UserRepository extends JpaRepository<UserDAO, Integer> {
-
-    UserDAO findById(int id);
-
-    UserDAO deleteById(int id);
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
 }
