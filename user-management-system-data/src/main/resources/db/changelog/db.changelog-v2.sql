d7df06c0805698b646d61e41d0dea99803d55854
Henry-Azer <henryazer@outlook.com>
2022-04-22 21:46:09 +0200

    exposing changelog files

---
+++ user-management-system-data/src/main/resources/db/changelog/db.changelog-v2.sql
@@ -0,0 +1,1 @@
+create table user (id bigint not null auto_increment, created_date datetime(6), modified_date datetime(6), address varchar(255), age integer, name varchar(255) not null, phone_number varchar(255) not null, primary key (id)) engine=InnoDB;
