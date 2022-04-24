ea042e20ceccb2f43a52dd32b36686335ecf7d0c
Henry-Azer <henryazer@outlook.com>
2022-04-24 23:30:32 +0200

    update user rest controller

--- user-management-system-backend/src/main/java/org/henry/controller/UserRestController.java
+++ user-management-system-backend/src/main/java/org/henry/controller/UserRestController.java
@@ -3,10 +3,12 @@ package org.henry.controller;
 import org.henry.dto.UserDto;
 import org.henry.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/user")
@@ -21,28 +23,40 @@ public class UserRestController {
 
     @GetMapping("/all")
     public ResponseEntity<List<UserDto>> getAllUsers() {
-        return ResponseEntity.ok(userService.findAll());
+        List<UserDto> users = userService.findAll();
+
+        return users.isEmpty() ?
+                new ResponseEntity<>(HttpStatus.NO_CONTENT) :
+                new ResponseEntity<>(users, HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<UserDto> getUser(@PathVariable Long id) {
-        return ResponseEntity.ok(userService.findById(id));
+        Optional<UserDto> user = Optional.ofNullable(userService.findById(id));
+
+        return user.map(dto -> new ResponseEntity<>(dto, HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
     @PostMapping
     public ResponseEntity<UserDto> createUser(@RequestBody UserDto user) {
-        return ResponseEntity.ok(userService.create(user));
+        return new ResponseEntity<>(userService.create(user), HttpStatus.CREATED);
     }
 
     @PutMapping
     public ResponseEntity<UserDto> updateUser(@RequestBody UserDto userDto) {
-        return ResponseEntity.ok(userService.update(userDto, userDto.getId()));
+        Optional<UserDto> user = Optional.ofNullable(userService.findById(userDto.getId()));
+
+
+        return user.isPresent() ?
+                new ResponseEntity<>(userService.update(userDto, userDto.getId()), HttpStatus.OK) :
+                new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<?> deleteUser(@PathVariable Long id) {
         userService.deleteById(id);
-        return ResponseEntity.ok("Deleted Successfully!");
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
 }
